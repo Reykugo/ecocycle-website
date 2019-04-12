300a7b170a153137f1bed82cbe5d8edfe0d71d6d
Guillaume <guillaume.nougier@ynov.com>
2019-04-12 12:08:55 +0200

    fix team images and add witch screen on smartphone

--- src/components/jean-fekoi/JeanFekoi.js
+++ src/components/jean-fekoi/JeanFekoi.js
@@ -5,22 +5,59 @@ import t from '../../utils/locales/Locales';
 import TextBlock from '../commons/TextBlock';
 
 class ShareCo extends Component {
+    constructor(props){
+        super(props)
+        this.screens = [
+            require('../../images/1.jpg'),
+            require('../../images/2.png'),
+            require('../../images/3.png'),
+            require('../../images/4.png'),
+            require('../../images/5.png'),
+            require('../../images/6.png'),
+            require('../../images/07.png'),
+            require('../../images/07.2.png'),
+            require('../../images/08.png'),
+            require('../../images/09.png'),
+            require('../../images/10.png'),
+            require('../../images/11.png'),
+        ]
+        this.state = {
+            currentScreen: this.screens[0],
+            screenIndex:0
+        }
+    }
+
+    componentDidMount(){
+        let self = this;
+        setInterval(() => {
+            self.getNewScreen()
+        }, 2000);
+    }
+
+    getNewScreen(){
+        let index = this.state.screenIndex + 1;
+        if(index < this.screens.length){
+            this.setState({currentScreen: this.screens[index], screenIndex: index});
+        }else{
+            this.setState({currentScreen: this.screens[0], screenIndex:0});
+        }
+    }
     render() {
         return (
             <section id="eco-scan" className="product-block eco-scan flex-row flex-center">
                 <div className="img-container flex-column flex-center">
                     <div className="smartphone-container">
                         <img className= "smartphone" src={require("../../images/smartphone.png")} alt="" />
-                        <img className="screen" src={require('../../images/jeanFekoi-home.jpg')}/>
+                        <img className="screen" src={this.state.currentScreen}/>
                     </div>
                 </div>
                 <div className="description-container flex-column flex-center">
                     <div className="description">
                         <h1 className="title">{t('Jean-Fekoi?')}</h1>
-                        <p className="text">
+                        <div className="text">
                             <TextBlock title="Description" iconClass="fa fa-info" text="jeanFekoi-description" />
                             <TextBlock title="Utilité" iconClass="fa fa-plus" text="jeanFekoi-utility" />
-                        </p>
+                        </div>
                     </div>
                 </div>
             </section>
